2d66de2f87b2f321d688a5a00548c651eb3340b9
Emmanuel Hernandez <snipa.v1@gmail.com>
2020-08-26 19:56:10 -0500

    Added geolocation

--- ip.py
+++ ip.py
@@ -1,22 +1,59 @@
 import requests
+import argparse
+import sys
 from termcolor import colored
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-c", "--city",
+                    dest="home_city",
+                    help="Set home city",
+                    action='store')
+args = parser.parse_args()
+home_city = args.home_city if args.home_city else None
 
-def print_ip(ip):
-    if ip['cc'] != 'US':
-        ip_address = colored(ip['ip'], 'green')
-        country = colored(ip['country'], 'green')
-        cc = colored(ip['cc'], 'green')
+def get_ip(response):
+    return response['ip']
+
+
+def print_location(geo_response, city):
+    if geo_response['city'].lower() == city.lower():
+        print(colored('-------------------------', 'red'))
+        print(colored('[*] NOT Connected to VPN', 'red'))
+        print(colored('-------------------------', 'red'))
+        print('Country: ', colored(geo_response['country'], 'red'))
+        print('Country Code: ', colored(geo_response['countryCode'], 'red'))
+        print('Region: ', colored(geo_response['region'], 'red'))
+        print('Region Name: ', colored(geo_response['regionName'], 'red'))
+        print('City: ', colored(geo_response['city'], 'red'))
+        print('ISP: ', colored(geo_response['isp'], 'red'))
+        print('Org: ', colored(geo_response['org'], 'red'))
     else:
-        ip_address = colored(ip['ip'], 'blue')
-        country = colored(ip['country'], 'blue')
-        cc = colored(ip['cc'], 'blue')
+        print(colored('---------------------', 'green'))
+        print(colored('[.] Connected to VPN', 'green'))
+        print(colored('---------------------', 'green'))
+        print('Country: ', colored(geo_response['country'], 'green'))
+        print('Country Code: ', colored(geo_response['countryCode'], 'green'))
+        print('Region: ', colored(geo_response['region'], 'green'))
+        print('Region Name: ', colored(geo_response['regionName'], 'green'))
+        print('City: ', colored(geo_response['city'], 'green'))
+        print('ISP: ', colored(geo_response['isp'], 'green'))
+        print('Org: ', colored(geo_response['org'], 'green'))
 
-    print(f"Public IP Address: {ip_address}")
-    print(f"Country: {country}")
-    print(f"CC: {cc}")
+
+def main(home_city=home_city):
+    if not home_city:
+        print(parser.print_help())
+        sys.exit('[!] Please provide a city\n')
+
+    ip_response = requests.get('https://api.myip.com').json()
+
+    ip_address = get_ip(ip_response)
+
+    geo_response = requests.get('http://ip-api.com/json/{}'.format
+                                (ip_address)).json()
+
+    print_location(geo_response, home_city)
 
 
-ip = requests.get('https://api.myip.com').json()
-
-print_ip(ip)
+if __name__ == '__main__':
+    main(home_city=home_city)
